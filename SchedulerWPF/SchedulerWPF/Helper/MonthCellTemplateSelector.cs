1e1cb7e2e47c32b012b2bd6e0c0e5b6a9238df3a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-06-14 16:26:41 +0530

    WPF-58244 - Sample added

---
+++ SchedulerWPF/SchedulerWPF/Helper/MonthCellTemplateSelector.cs
@@ -0,0 +1,70 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using WpfScheduler.ViewModel;
+
+namespace WpfScheduler
+{
+    public class MonthCellTemplateSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MonthCellTemplateSelector" /> class.
+        /// </summary>
+        public MonthCellTemplateSelector()
+        {
+            var mainWindow = App.Current.MainWindow as Window;
+            this.MonthAppointmentTemplate = mainWindow.Resources["monthAppointmentTemplate"] as DataTemplate;
+            this.MonthCellDatesTemplate = mainWindow.Resources["monthCellTemplate"] as DataTemplate;
+        }
+
+        /// <summary>
+        /// Gets or sets Month Appointment Template.
+        /// </summary>
+        public DataTemplate MonthAppointmentTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets Month Cell Dates Template.
+        /// </summary>
+        public DataTemplate MonthCellDatesTemplate { get; set; }
+
+        /// <summary>
+        /// Template selection method
+        /// </summary>
+        /// <param name="item">return the object</param>
+        /// <param name="container">return the bindable object</param>
+        /// <returns>return the template</returns>
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+
+            var appointments = item as List<ScheduleAppointment>;
+            var cell = container as MonthCell;
+
+            if (cell.DateTime.Date == DateTime.Now.Date)
+                cell.Foreground = Brushes.Black;
+            if (appointments == null || appointments.Count == 0)
+            {
+                cell.DataContext = cell;
+                return MonthCellDatesTemplate;
+            }
+            else
+            {
+                MonthCellViewModel monthCellViewModel = new MonthCellViewModel();
+                monthCellViewModel.Foreground = cell.Foreground;
+                monthCellViewModel.DateText = cell.DateText;
+                monthCellViewModel.AppointmentCount = appointments.Count.ToString();
+                cell.DataContext = monthCellViewModel;
+                return MonthAppointmentTemplate;
+            }
+        }
+    }
+}
+       
+    
+
+
+
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/MonthCellViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfScheduler.ViewModel
+{
+    public class MonthCellViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        /// <summary>
+        /// Property changed event handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Gets or sets month foreground.
+        /// </summary>
+        public Brush Foreground { get; set; }
+        /// <summary>
+        /// Gets or sets month date.
+        /// </summary>
+        public string DateText { get; set; }
+        /// <summary>
+        /// Gets or sets month cell appointments.
+        /// </summary>
+        public string AppointmentCount { get; set; }
+        #endregion
+        #region Property Changed Event
+        /// <summary>
+        /// Invoke method when property changed
+        /// </summary>
+        /// <param name="propertyName">property name</param>
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
